bfcd83184fd86c991097a8185a32acb131e658a8
1Lucas-Matheussm <1lucasmatheus.silvam@gmail.com>
2025-06-03 09:52:00 -0300

    Configuracao das migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'type' => 1,
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('admin123'),
         ]);
+
+        User::factory(10)->create();
     }
 }
